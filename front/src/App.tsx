17f77572c04e248c0dba18ad2b73c47c4074aa32
tofdino <tofdino@tofdino.local>
2020-08-10 10:41:18 +0900

    front

---
+++ front/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+    const [val, set] = useState("");
+    const [phrase, setPhrase] = useState("example phrase");
+
+    const createPhrase = () => {
+        setPhrase(val);
+        set("");
+    };
+
+    useEffect(() => {
+        console.log(`typing "${val}"`);
+    },[val]);
+
+    useEffect(() => {
+        console.log(`saved phrase: "${phrase}"`);
+    }, [phrase]);
+
+
+
+    return (
+        <>
+            <label>Favorite phrase:</label>
+            <input
+                value={val}
+                placeholder={phrase}
+                onChange={e => set(e.target.value)}
+            />
+            <button onClick={createPhrase}>send</button>
+        </>
+    );
+}
+
+export default App;
